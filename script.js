d601d0d3f378310f838db1cf44ad4cb41f6c6540
Jp21212 <Jamesp2814@tuckahoeschools.org>
2024-11-12 13:22:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const submitButton = document.querySelector('submit')
+const header = document.querySelector('header')
+const score = document.querySelector('score')
+const questions = document.querySelector('questions')
+const inputContainer = document.querySelector('input-container')
+const outputContainer = document.querySelector('output-container')
+const submitSwitch = document.querySelector('submit-switch')
+submitSwitch.addEventListener("click", ()=>{
+    document.getElementById('name').value = '';
+
+    currentQuestionIndex = (currentQuestionIndex)
+})
