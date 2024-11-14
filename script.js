28c18500f27a5d4455c8323e5e030d96a1faac64
Jp21212 <Jamesp2814@tuckahoeschools.org>
2024-11-14 13:31:33 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-const submitButton = document.querySelector('submit')
-const header = document.querySelector('header')
-const score = document.querySelector('score')
-const questions = document.querySelector('questions')
-const inputContainer = document.querySelector('input-container')
-const outputContainer = document.querySelector('output-container')
-const submitSwitch = document.querySelector('submit-switch')
-submitSwitch.addEventListener("click", ()=>{
-    document.getElementById('name').value = '';
+let questionList = ['Whats bobs favrotie school object?', 'Whats coopers first name?', 'Does Cooper like us or engineering more?', 'Whats the capatial of Columbia?', 'Whats the capital of California?', 'Whats the capital of Colorado?', 'Whats the capital of Georgia?', 'Whats the capital of Arizona?', 'Whats the capital of Idaho?','Whats the capital of Massachusetts?', 'Whats the capital of Nebraska?', 'Whats the capital of Nevada?']
+let answerList = ['Pencil', 'Louis', 'Us', 'Bogota', 'Sacramento', 'Denver', 'Atlanta', 'Phoenix', 'Boise', 'Boston', 'Lincoln', 'Carson City',];
+let score = 0;
+let questionIndex = 0;
 
-    currentQuestionIndex = (currentQuestionIndex)
-})
-
-const questionArray = [
-    "Whats bobs favrotie school object"
-    ""
-
-]
+const question = document.querySelector('.question-container');
+const button = document.querySelector('.button-container');
+const input = document.querySelector('#input-field');
